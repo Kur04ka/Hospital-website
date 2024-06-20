541cb956b7c552926f2d70b10b088cc52bbe1107
Kur04ka <vanya.kurochkin.23@mail.ru>
2024-06-20 14:57:19 +0700

    alert added

--- frontend/src/views/components/home/leave_phone_form/index.tsx
+++ frontend/src/views/components/home/leave_phone_form/index.tsx
@@ -5,21 +5,22 @@ import { MuiTelInput } from 'mui-tel-input';
 import { instance } from '../../../../utils/axios';
 import { isValidPhoneNumber } from 'libphonenumber-js';
 import DialogAccepted from './dialogAccepted';
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
 
 const LeavePhoneForm = () => {
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
-  const [phoneError, setPhoneError] = useState(false);
   const [open, setOpen] = useState<boolean>(false);
+  const [openSnackBar, setSnackBarOpen] = useState<boolean>(false);
 
   const handleSubmit = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
 
     if (!isValidPhoneNumber(phone)) {
-      setPhoneError(true);
+      setSnackBarOpen(true)
       return;
     }
-    setPhoneError(false);
 
     try {
       const callRequestData = {
@@ -39,6 +40,14 @@ const LeavePhoneForm = () => {
     }
   };
 
+  const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setSnackBarOpen(false);
+  };
+
   return (
     <div className={styles.main}>
       <Typography gutterBottom variant="h3" sx={{ color: 'rgba(8, 44, 77, 1)', marginBottom: 3 }}>Есть вопросы по здоровью?<br />Специалисты подскажут и помогут!</Typography>
@@ -47,22 +56,21 @@ const LeavePhoneForm = () => {
       <form className={styles.form} onSubmit={handleSubmit}>
         <TextField className={styles.item} onChange={(e) => setName(e.target.value)} value={name} required label="Имя" placeholder="Введите ваше Имя" />
         <div className={styles.item}>
-          <MuiTelInput 
-            onChange={(newValue) => setPhone(newValue)} 
-            value={phone} 
-            defaultCountry='RU' 
-            label="Телефон" 
-            required 
-            error={phoneError}
+          <MuiTelInput
+            onChange={(newValue) => setPhone(newValue)}
+            value={phone}
+            defaultCountry='RU'
+            label="Телефон"
+            required
           />
-          {phoneError && (
-            <FormHelperText error>Неверный номер телефона</FormHelperText>
-          )}
+          <Snackbar open={openSnackBar} autoHideDuration={6000} onClose={handleClose}>
+            <Alert severity="error" onClose={handleClose} sx={{ width: '100%' }}>Неверный номер телефона</Alert>
+          </Snackbar>
         </div>
         <Button type='submit' className={`${styles.button} ${styles.item}`} variant="contained">Оставить заявку</Button>
       </form>
 
-        <DialogAccepted open={open} setOpen={setOpen}/>
+      <DialogAccepted open={open} setOpen={setOpen} />
     </div>
   );
 };
